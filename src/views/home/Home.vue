d1f763bac46f9ecce1c015b91a8a99645f3f208c
wolf-lang <github.com/wolf-lang>
2022-05-02 12:41:17 +0800

    首页

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,33 +1,38 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav">
-      <div slot="center">购物街</div>
-    </nav-bar>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
   </div>
-
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar';
-  import {getHomeMultidata} from 'network/home';
+  import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/RecommendView'
+
+  import {getHomeMultidata} from "network/home";
+
   export default {
     name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView
+    },
     data() {
       return {
         banners: [],
         recommends: []
       }
     },
-    components: {
-      NavBar,
-    },
     created() {
-      // 请求多个数据
+      // 1.请求多个数据
       getHomeMultidata().then(res => {
-        console.log(res);
+        // this.result = res;
         this.banners = res.data.banner.list;
         this.recommends = res.data.recommend.list;
-      });
+      })
     }
   }
 </script>
@@ -35,7 +40,6 @@
 <style scoped>
   .home-nav {
     background-color: var(--color-tint);
-    color: #f1f1f1;
-    font-size: 16px;
+    color: #fff;
   }
 </style>
